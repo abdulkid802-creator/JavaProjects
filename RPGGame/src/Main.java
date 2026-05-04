02977b94a39cffba0b3858a7f6d775abd9e761d2
Abdul <abdulkid802@gmail.com>
2026-05-04 23:54:35 +0100

    Java Projects

--- RPGGame/src/Main.java
+++ RPGGame/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
 
-        Player player = new Player("Abdul");
+        Player player = new Player("Abdul"); // Change to whatever you want your player to be called
         Item item = new Item("Health Potion", "Potion", 30);
         Item item1 = new Item("Iron Sword", "Weapon", 10);
 
